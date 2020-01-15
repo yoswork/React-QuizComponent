506575a74b4684b02fdbb771c9befda0cbf7732d
Yos Noor <yos.noor@wwt.com>
2020-01-15 12:50:48 +0000

    Module7

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length);
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> :
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> :
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         );
@@ -24,6 +24,12 @@ class Quiz extends Component {
     showNextQuestion() {
         this.setState((state) => {
             return {quiz_position: state.quiz_position+1};
+        })
+    }
+
+    handleResetClick() {
+        this.setState(() => {
+            return {quiz_position: 1};
         })
     }
 }
