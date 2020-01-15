32c58bf9779455a954c2158d2030f164245a507b
Yos Noor <yos.noor@wwt.com>
2020-01-15 12:06:38 +0000

    Module3

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import QuizQuestionButton from './QuizQuestionButton.js';
+
 class QuizQuestion extends Component {
     render() {
         return (
@@ -9,10 +11,7 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>
-                            {this.props.quiz_question.answer_options[0]}
-                        </li>
-                        //quiz question button logic goes here
+                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
                     </ul>
                 </section>
         </main>
