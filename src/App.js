0d910e0e8f044643ea2c21f504db7175f0763305
Yos Noor <yos.noor@wwt.com>
2020-01-15 11:43:37 +0000

    Module 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
